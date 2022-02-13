647fbf73dff60f228d879800b560e05036e6fc41
Jaeha Choi <jaeha@mail.jaeha.dev>
2022-02-13 01:38:54 -0800

    Add api key parameter

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/bash
 
 # Just run
-# java -cp ".:lib/commons-cli-1.5.0.jar:lib/gson-2.8.9.jar:" MyCity.java -s WA Seattle
+# java -cp ".:lib/commons-cli-1.5.0.jar:lib/gson-2.8.9.jar:" MyCity.java -s WA Seattle *api_key*
 
 # Build
-javac -cp ".:lib/commons-cli-1.5.0.jar:lib/gson-2.8.9.jar:" MyCity.java
+javac -cp ".:lib/commons-cli-1.5.0.jar:lib/gson-2.8.9.jar:" MyCity.java *api_key*
 # Run
-java -cp ".:lib/commons-cli-1.5.0.jar:lib/gson-2.8.9.jar:" MyCity -s WA Seattle
+java -cp ".:lib/commons-cli-1.5.0.jar:lib/gson-2.8.9.jar:" MyCity -s WA Seattle *api_key*
